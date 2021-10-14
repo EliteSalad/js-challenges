4f208eed9ebfeefec1a88c7fc9abcbf7be871a7e
EliteSalad <32502593+EliteSalad@users.noreply.github.com>
2021-10-14 22:57:26 +0100

    switch assignment started

---
+++ Grade-Calculator/dist/script.dev.js
@@ -0,0 +1,36 @@
+"use strict";
+
+function handleCalculateGrade(event) {
+  var grade = event.target.value; //on blur event find target (text box) and get the value component
+
+  var percentage;
+  grade.toUpperCase();
+
+  switch (grade) {
+    case "A":
+      percentage = "90-100%";
+      break;
+
+    case "B":
+      percentage = "75-90%";
+      break;
+
+    case "C":
+      percentage = "50-75%";
+      break;
+
+    case "D":
+      percentage = "25-50%";
+      break;
+
+    case "E":
+      percentage = "0-25%";
+      break;
+
+    default:
+      alert(" please enter valid grade (A-E)");
+  }
+
+  alert("The precentage was " + percentage);
+}
+//# sourceMappingURL=script.dev.js.map
