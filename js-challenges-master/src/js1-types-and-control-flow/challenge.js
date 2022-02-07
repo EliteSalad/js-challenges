3a3ab44fd858889233b46c6bf2439a818e5f4a29
EliteSalad <32502593+EliteSalad@users.noreply.github.com>
2022-02-07 14:43:49 +0000

    challenges 1 updated

--- js-challenges-master/src/js1-types-and-control-flow/challenge.js
+++ js-challenges-master/src/js1-types-and-control-flow/challenge.js
@@ -15,120 +15,161 @@
  *
  * @returns {string} John Smith
  */
-const firstName = "John";
-const lastName = "Smith";
-
-export const createFullName = () => {
-  // Write your code here
-};
-
-/**
- * A function that programmatically returns the largest number.
- * This means if the numbers were different it would still return the largest one.
- *
- * @returns {number} the largest number
- */
-const largeNumber1 = 100;
-const largeNumber2 = 200;
-
-export const findLargestNumber = () => {
-  // Write your code here
-};
-
-/**
- * A function that programmatically adds two numbers together.
- * This means if the numbers were different it would still add them together.
- *
- * @returns {number} the sum of both numbers
- */
-const addNumber1 = 12;
-const addNumber2 = 24;
-
-export const addNumbers = () => {
-  // Write your code here
-};
-
-/* Intermediate Challenges */
-
-/**
- * A function to programmatically find the length of a string.
- * This means if the string was different it would still find the length.
- *
- * @returns {number} the length of the string
- */
-const password = "thisIsMyVeryLongPassword123456789";
-
-export const findLengthOfPassword = () => {
-  // Write your code here
-};
-
-/**
- * A function that programmatically tells you what the type of the variable is.
- * This variable was still one of the accepted types it would still output a string.
- * If the variable is a string output "This is a string"
- * If the variable is a number output "This is a number"
- * If the variable is a boolean output "This is a boolean"
- * If the variable is not any of those types output "I don't know what this thing is"
- *
- * @returns {string} This is a string
- */
-const thing = "I am a thing";
-
-export const findType = () => {
-  // Write your code here
-};
-
-/**
- * A function to programmatically decide if a name is suitable for a name tag.
- * This means it must still work even if the name is different and return something if name provided is incorrect.
- * Name tag rules are: The name must be less than or equal to 8 characters and begin with a capital letter.
- *
- * @returns {boolean} true || false
- */
-const nameTagOption = "Timothy";
-
-export const getIsValidOnNameTag = () => {
-  // Write your code here
-};
-
-/* Advanced Challenges */
-
-/**
- * A function that programmatically converts a number stored as a string to a number type.
- * This means if the string was different it would still convert it to a number.
- *
- * @returns {number} The number converted from the string "32.87" -> 32.87
- */
-const stringToConvert = "14.45";
-
-export const convertStringToNumber = () => {
-  // Write your code here
-};
-
-/**
- * A function that uses regex to programmatically test if a string contains uppercase letters.
- * This means if the string was different it would still accurately return true or false.
- *
- * !!NOTE!! You should use regex to solve this problem!
- *
- * @returns {boolean} true || false
- */
-const stringWithUppercaseLetters = "I Am A String With Uppercase Letters";
-
-export const getHasUppercaseLetters = () => {
-  // Write your code here
-};
-
-/* Expert Challenge */
-
-/**
- * A function that programmatically converts a string written in PascalCase to a string written in snake_case (lowercase with underscores).
- * This means if the PascalCase string changes then the function should still convert it.
- *
- * @returns {string} i_want_to_be_snake_case
- */
-const pascalCaseVariableName = "IWantToBeSnakeCase";
-
-export const convertPascalCaseToSnakeCase = () => {
-  // Write your code here
-};
+ const firstName = "John";
+ const lastName = "Smith";
+ 
+ export const createFullName = () => {
+   return `${firstName} ${lastName}`;
+   // Write your code here
+ };
+ 
+ /**
+  * A function that programmatically returns the largest number.
+  * This means if the numbers were different it would still return the largest one.
+  *
+  * @returns {number} the largest number
+  */
+ const largeNumber1 = 100;
+ const largeNumber2 = 200;
+ 
+ export const findLargestNumber = () => {
+   return (largeNumber1 > largeNumber2 ? largeNumber1 : largeNumber2);
+   // Write your code here
+ };
+ 
+ /**
+  * A function that programmatically adds two numbers together.
+  * This means if the numbers were different it would still add them together.
+  *
+  * @returns {number} the sum of both numbers
+  */
+ const addNumber1 = 12;
+ const addNumber2 = 24;
+ 
+ export const addNumbers = () => {
+   return addNumber1 + addNumber2;
+   // Write your code here
+ };
+ 
+ /* Intermediate Challenges */
+ 
+ /**
+  * A function to programmatically find the length of a string.
+  * This means if the string was different it would still find the length.
+  *
+  * @returns {number} the length of the string
+  */
+ const password = "thisIsMyVeryLongPassword123456789";
+ 
+ export const findLengthOfPassword = () => {
+   return password.length;
+   // Write your code here
+ };
+ 
+ /**
+  * A function that programmatically tells you what the type of the variable is.
+  * This variable was still one of the accepted types it would still output a string.
+  * If the variable is a string output "This is a string"
+  * If the variable is a number output "This is a number"
+  * If the variable is a boolean output "This is a boolean"
+  * If the variable is not any of those types output "I don't know what this thing is"
+  *
+  * @returns {string} This is a string
+  */
+ const thing = "I am a thing";
+ 
+ export const findType = () => {
+   const thisThing = typeof thing;
+   if (thisThing === "string") 
+     return "This is a string";
+   else if (thisThing === "number") 
+     return "This is a number";
+   else if (thisThing === "boolean") 
+     return "This is a boolean";
+   else 
+     return "I don't know what this thing is";
+   
+   // Write your code here
+ };
+ 
+ /**
+  * A function to programmatically decide if a name is suitable for a name tag.
+  * This means it must still work even if the name is different and return something if name provided is incorrect.
+  * Name tag rules are: The name must be less than or equal to 8 characters and begin with a capital letter.
+  *
+  * @returns {boolean} true || false
+  */
+ const nameTagOption = "Timothy";
+ 
+ export const getIsValidOnNameTag = () => {
+ 
+   const firstLetterOfName = nameTagOption.substring(0, 1);
+   const isValidName = nameTagOption.length <= 8 && firstLetterOfName === firstLetterOfName.toUpperCase();
+ 
+   return isValidName;
+   
+   const isCap = (nameTagOption.substring(0,1) === nameTagOption.toUpperCase()? true : false);
+   const isLength = (nameTagOption.length < 8 ? true : false );
+   return (isCap && isLength? true : false);
+   // Write your code here
+ };
+ 
+ /* Advanced Challenges */
+ 
+ /**
+  * A function that programmatically converts a number stored as a string to a number type.
+  * This means if the string was different it would still convert it to a number.
+  *
+  * @returns {number} The number converted from the string "32.87" -> 32.87
+  */
+ const stringToConvert = "14.45";
+ 
+ export const convertStringToNumber = () => {
+   const stringNumber = parseFloat(stringToConvert);
+   return stringNumber;
+   // Write your code here
+ };
+ 
+ /**
+  * A function that uses regex to programmatically test if a string contains uppercase letters.
+  * This means if the string was different it would still accurately return true or false.
+  *
+  * !!NOTE!! You should use regex to solve this problem!
+  *
+  * @returns {boolean} true || false
+  */
+ const stringWithUppercaseLetters = "I Am A String With Uppercase Letters";
+ 
+ export const getHasUppercaseLetters = () => {
+   const regex = /([A-Z])/g;                                             //what is regex
+   const hasUpperCaseLetters = regex.test(stringWithUppercaseLetters);
+ 
+   return hasUpperCaseLetters;
+   
+   Array.from(stringWithUppercaseLetters).forEach(element => {
+     if(element === element.toUpperCase())
+     return true; 
+   });
+   return false;
+ 
+ 
+ };
+ 
+ /* Expert Challenge */
+ 
+ /**
+  * A function that programmatically converts a string written in PascalCase to a string written in snake_case (lowercase with underscores).
+  * This means if the PascalCase string changes then the function should still convert it.
+  *
+  * @returns {string} i_want_to_be_snake_case
+  */
+ const pascalCaseVariableName = "IWantToBeSnakeCase";
+ 
+ export const convertPascalCaseToSnakeCase = () => {
+ 
+   const splitWords = pascalCaseVariableName.split(/(?=[A-Z])/); //split where there is a capital
+   const joinedWordsWithUnderscores = splitWords.join("_");      //join split with underscore
+   const snakeCase = joinedWordsWithUnderscores.toLowerCase();   //convert everything to lower case
+ 
+   return snakeCase;   
+ };
